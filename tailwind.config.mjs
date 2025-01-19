881fd644a5e4e2b247a55748f1e4f6621c3857aa
KaushalPatel14 <kn.patel1214@gmail.com>
2025-01-20 00:36:56 +0530

    Add files via upload
    set to app router

---
+++ tailwind.config.mjs
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+
+      },
+      colors: {
+        lightBlack: "var(--lightBlack)"
+      },
+
+      fontFamily: {
+        Dosis: "Dosis , serif",
+        OpenSans: "Open Sans, serif"
+      },
+
+      screens: {
+        lsm: "350px",
+        esm: "400px",
+        em: "480px",
+        ew: "510px",
+        vem: "560px",
+        sm: "640px",
+        md: "768px",
+        emd: "951px",
+        lg: "1024px",
+        xlg: "1150px",
+        xl: "1280px",
+        "1xl": "1440px",
+        "2xl": "1530px",
+        "3xl": "1600px",
+        "4xl": "1920px",
+      },
+    },
+  },
+  plugins: [],
+};
