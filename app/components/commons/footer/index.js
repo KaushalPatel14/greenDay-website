cac2da45c469f40456da9db3735d650b2d049462
KaushalPatel14 <kn.patel1214@gmail.com>
2025-01-21 22:26:24 +0530

    path change

--- app/components/commons/footer/index.js
+++ app/components/commons/footer/index.js
@@ -1,5 +1,5 @@
 "use client"
-import { footerData } from '@/lib/json/layoutData'
+import { footerData } from '../../../../lib/json/layoutData/index'
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
