765d864f9efa09f45129a552b57a05611f6a2670
KaushalPatel14 <kn.patel1214@gmail.com>
2025-02-05 22:01:58 +0530

    image error solve

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,10 @@ const nextConfig = {
         NEXT_APP_API_URL: process.env.NEXT_APP_API_URL,
         NEXT_X_API_KEY : process.env.NEXT_X_API_KEY,
         NEXT_TOKEN : process.env.NEXT_TOKEN
-    }
+    },
+    images: {
+        domains: ['your-image-source.com'],
+      },
 };
 
 export default nextConfig;
