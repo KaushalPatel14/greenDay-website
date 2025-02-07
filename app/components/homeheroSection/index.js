3b7ddf43d75a6cd2b43aa44e87225be3d2cb5091
KaushalPatel14 <kn.patel1214@gmail.com>
2025-02-08 00:05:35 +0530

    hero image loading set
    hero image loading set

---
+++ app/components/homeheroSection/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Button from '../commons/button';
+import Image from 'next/image';
+
+const HomeHeroSection = ({ bannerData }) => {
+    return (
+        <div>
+            <div className=''>
+                {bannerData && bannerData.map((item, index) => {
+                    const { title, description, Titletag, button, image, link, variant } = item;
+                    const TitleTagComponent = Titletag || 'h2';
+
+                    return (
+                        <div key={index} className='flex flex-col items-center justify-center w-full relative z-10' >
+                            <div className='bg-black/30 py-[210px] md:py-[300px] lg:py-[358px] px-8 w-full max-w-[1920px] absolute inset-0 -z-10'>
+                            </div>
+                            <div className='w-full max-w-[983px] h-full min-h-[880px] mx-auto flex flex-col items-center justify-center'>
+                                {title && <TitleTagComponent className='text-center font-bold font-Dosis text-white' >{title}</TitleTagComponent>}
+                                {description && <p className='text-white text-center font-OpenSans SecondaryDescription pt-[30px] w-full max-w-[648px]'>{description}</p>}
+                                {button &&
+                                    <div className='pt-[40px]' >
+                                        <Button label={button} href={link} variant={variant} />
+                                    </div>
+                                }
+                            </div>
+                            <Image src={image} alt='image' width={1920} height={1063} className='absolute inset-0 w-full max-w-[1920px] h-[1063px] -z-10' />
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default HomeHeroSection
--- app/page.js
+++ app/page.js
@@ -8,14 +8,12 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import { banner, homeMeassageSection, homeTestimonial, PriceRatedata, welcomDaySpa } from "../lib/json/pagesData/homePage/index";
-import Banner from "./(main)/components/commons/banner";
-import GreenDaySpaSection from "./(main)/components/greenDaySpaSection";
-import MeassageSection from "./(main)/components/meassageSection";
-import PriceRateSection from "./(main)/components/priceRateSection";
-import TestimonialSection from "./(main)/components/testimonialSection";
-import Footer from "./(main)/components/commons/footer";
-
-import { motion } from 'framer-motion';
+import GreenDaySpaSection from "./components/greenDaySpaSection";
+import MeassageSection from "./components/meassageSection";
+import PriceRateSection from "./components/priceRateSection";
+import TestimonialSection from "./components/testimonialSection";
+import Footer from "./components/commons/footer";
+import HomeHeroSection from"../app/components/homeheroSection/index"
 
 export default function Home() {
   return (
@@ -30,67 +28,44 @@ export default function Home() {
         loop={true}
         mousewheel={true}
         modules={[EffectFade, Mousewheel,Autoplay]}
-        className="h-screen"
+        className=""
         speed={500}
       >
         <SwiperSlide>
-          <motion.div
-            // initial={{ opacity: 0 }}
-            // animate={{ opacity: 1 }}
-            // transition={{ duration: 1 }}
-          >
-            <Banner bannerData={banner} />
-          </motion.div>
+       
+            {/* <Banner bannerData={banner} /> */}
+            <HomeHeroSection bannerData={banner} />
+     
         </SwiperSlide>
 
         <SwiperSlide>
-          <motion.div
-            // initial={{ opacity: 0 }}
-            // animate={{ opacity: 1 }}
-            // transition={{ duration: 1 }}
-          >
-            <GreenDaySpaSection GreenDaySpaData={welcomDaySpa} />
-          </motion.div>
+     <GreenDaySpaSection GreenDaySpaData={welcomDaySpa} />
+
         </SwiperSlide>
 
         <SwiperSlide>
-          <motion.div
-            // initial={{ opacity: 0 }}
-            // animate={{ opacity: 1 }}
-            // transition={{ duration: 1 }}
-          >
+     
             <MeassageSection meassageSectionData={homeMeassageSection} />
-          </motion.div>
+   
         </SwiperSlide>
 
         <SwiperSlide>
-          <motion.div
-            // initial={{ opacity: 0 }}
-            // animate={{ opacity: 1 }}
-            // transition={{ duration: 1 }}
-          >
+     
             <TestimonialSection TestimonialData={homeTestimonial} />
-          </motion.div>
+       
         </SwiperSlide>
 
         <SwiperSlide>
-          <motion.div
-            // initial={{ opacity: 0 }}
-            // animate={{ opacity: 1 }}
-            // transition={{ duration: 1 }}
-          >
+   
             <PriceRateSection PriceRateSectiondata={PriceRatedata} />
-          </motion.div>
+   
         </SwiperSlide>
 
         <SwiperSlide>
-          <motion.div
-            // initial={{ opacity: 0 }}
-            // animate={{ opacity: 1 }}
-            // transition={{ duration: 1 }}
-          >
+  
+   
             <Footer />
-          </motion.div>
+     
         </SwiperSlide>
       </Swiper>
     </div>
